0884967bc1ef29e0b6f0345c53807df89af47a6a
Abhishek N N <7abhisheknn@gmail.com>
2023-03-30 11:17:23 +0530

    done

--- scrape/scrape_main.py
+++ scrape/scrape_main.py
@@ -1,7 +1,6 @@
 from scrape import naukri
 from scrape import simply_hired
 from dbms import mdb
-import time
 
 def getSkills(searchString, noOfPages=5):
     l=mdb.queryList(searchString)
@@ -9,6 +8,6 @@ def getSkills(searchString, noOfPages=5):
         return l
     skills = naukri.naukri(searchString, noOfPages)
     skills.extend(simply_hired.simplyHired(searchString, noOfPages))
-    
+
     mdb.insert(searchString, skills)
     return skills
